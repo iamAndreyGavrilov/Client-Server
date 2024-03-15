287de93939d387cf7de8f9c41f0e02c76b079945
A G <a.gavrilof@zordoc.uz>
2024-03-15 20:24:35 +0500

    feat: добавил getWaifu

--- src/components/clientServerApp/ClientServer.vue
+++ src/components/clientServerApp/ClientServer.vue
@@ -1,8 +1,68 @@
 <template>
-  <div>
-    <h1>This is a client-server app</h1>
+  <div class="client-server">
+    <div class="client-server__wrap">
+      <button class="client-server__btn" @click="getWaifu">Get Waifu</button>
+      <div class="client-server__img">
+        <img :src="img" alt="waifu" />
+      </div>
+    </div>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
 
-<style lang="scss"></style>
+const apiUrl = 'https://api.waifu.im/search'
+
+const img = ref('')
+
+const getWaifu = async () => {
+  try {
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+    if (response.ok) {
+      img.value = data.images.map((image: any) => {
+        return image.url
+      })
+    } else {
+      console.error('Error fetching waifu')
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
+</script>
+
+<style lang="scss">
+.client-server {
+  &_wrap {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  }
+
+  &__img {
+    display: flex;
+    justify-content: center;
+    margin-top: 2rem;
+  }
+
+  img {
+    width: 100%;
+    height: 550px;
+    border-radius: 20px;
+  }
+
+  &__btn {
+    display: block;
+    margin: 0 auto;
+    padding: 1rem 2rem;
+    font-size: 1.5rem;
+    border-radius: 10px;
+    background-color: var(--color-white);
+    color: greenyellow;
+    cursor: pointer;
+  }
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,9 +7,9 @@
 <style lang="scss">
 @media (min-width: 1024px) {
   .about {
-    //min-height: 100vh;
     display: flex;
     align-items: center;
+    justify-content: center;
   }
 }
 </style>
