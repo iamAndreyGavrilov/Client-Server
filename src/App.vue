26942cae965346719f4533e3c5b8ab0f498c2819
A G <a.gavrilof@zordoc.uz>
2024-03-15 17:06:47 +0500

    feat: добавил 02

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <div class="wrapper">
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </div>
 
   <RouterView />
 </template>
 
-<style scoped>
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<style scoped lang="scss">
 header {
   line-height: 1.5;
   max-height: 100vh;
@@ -39,7 +32,7 @@ nav {
 }
 
 nav a.router-link-exact-active {
-  color: var(--color-text);
+  color: hsla(160, 100%, 37%, 1);
 }
 
 nav a.router-link-exact-active:hover {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h1>this is the home view</h1>
+  </div>
 </template>
 
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
+<style lang="scss">
+@media (min-width: 1024px) {
+  .home {
+    //min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
